5560ba91d1e66235480eb944937cff5465830c14
open1234 <2506705328@qq.com>
2016-06-08 15:56:09 +0800

    博客结构修改

--- js/open1234.js
+++ js/open1234.js
@@ -1,58 +1,23 @@
 $(function () {
-    var $win_width = $(window).width();//窗口宽度
-    var $win_height = $(window).height();//窗口高度
-    var $img_width = $win_height / 4;//图片宽度
-    var $img_num = $("ul.accordion li a img").size();//图片数量
-
-    //菜单栏设置
-    setMenuHeight($win_height - 380);
-    setAccordionImgSize($img_width, $win_height);
-    $("ul.accordion").css("width", $img_num * $img_width);
-
-    //窗口发生变化
+    setImgSizeByWinSize();
     $(window).resize(function () {
-        var $win_height = $(window).height();
-        setMenuHeight($win_height - 380);
-        setAccordionImgSize($win_height / 4, $win_height);
+        setImgSizeByWinSize();
     });
-
-    //鼠标指针事件
-    $(window).mouseover(function (e) {
-        console.log("x:" + e.pageX + ",y:" + e.pageY);
-        var mouseX = e.pageX;
-        var mouseY = e.pageY;
-        //startAccordionAnimate();
-    });
-
-    //菜单颜色效果
-    //$("ul.menu li a").hover(
-    //    function () {
-    //        $(this).addClass("animated flipInX");
-    //    },
-    //    function () {
-    //        var node = $(this);
-    //        setTimeout(function () {
-    //            node.removeClass("animated flipInX");
-    //        }, 1000);
-    //    }
-    //);
 });
 
-//设置菜单栏高度
-function setMenuHeight(height) {
-    $(".menu").css("height", height);
+function setImgSizeByWinSize() {
+    var $win_height = $(document).height();
+    if ($win_height < 400) {
+        $win_height = 400;
+    }
+    if ($win_height > 1080) {
+        $win_height = 1080;
+    }
+    setImgSize($("div.image-list > a > img"), $win_height / 8, $win_height / 2);
 }
 
-//设置Accordion的图片大小
-function setAccordionImgSize(width, height) {
-    $("ul.accordion li a img").css("width", width);
-    $("ul.accordion li a img").css("height", height);
-}
-
-function startAccordionAnimate() {
-    setInterval(function () {
-        var $left = $("ul.accordion").css("left");
-        $left = Number($left.substring(0, $left.indexOf("px"))) + 1;
-        $("ul.accordion").css("left", $left + "px");
-    }, 100);
+/* 设置图片宽高 */
+function setImgSize($node, width, height) {
+    $node.css("width", width);
+    $node.css("height", height);
 }
